27146ef545e03f5474b2dde8ed7a9def5bff16b2
master <razafiarisonialy@gmail.com>
2020-03-11 11:18:32 +0400

    2eme modification

--- nan.py
+++ nan.py
@@ -1,20 +1,9 @@
-#!/usr/bin/env python
-
-
-import sys
-if _name_ == "_main_":
-      if len(sys.argv) > 3:
-          print ("erreur,")
-
-      elif len (sys.argv) == 3:
-          x = int(sys.argv[1])
-          y = int(sys.argv[2])
-          print ("somme = ", x+y)
-
-      else:
-          print("deux argument svp")
-          x = int(sys.argv[1])
-          y = int (input("y= ")
-          print ("somme = ", x+y)
+def main():
+    s=0
+    for i in range(0,2):
+        x = int(input("a= "))
+        s = s+ x
+    print("la somme est :", s)
+main()
 
 
