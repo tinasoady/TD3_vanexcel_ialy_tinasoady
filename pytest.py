99aa329eafc084389f3d9a9a525aedc7ac1eb9b6
tinasoady <jtinasoady@gmail.com>
2020-03-13 16:36:00 +0400

    2em modif

--- pytest.py
+++ pytest.py
@@ -1,17 +1,14 @@
 def main():
     S=0
     print ("argument = ", end='')
-    n= int(input())
-    if (n<2):
-        print ("Erreur! ne saisi pas l'argument plus de 2 ou moins")
-        return 0
-    if (n>2):
-        print ("Erreur! ne saisi pas l'argument plus de 2 ou moins")
-        return 0
+    n = int(input())
     if (n==2):
-        for i in range(1, 3):
-            print(i,"er argument est: ", end='')
-            a= int(input())
+        for i in range (1, 3):
+            print ("argument",i," = ", end='')
+            a = int(input())
             S= a+S
-        print(" la somme vaut : ", s) 
+        print("la somme vaut : ", S)
+    else :
+        print("Erreur! ")
+        main()
 main()
