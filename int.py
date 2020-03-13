9e5d3affecb619451fe0eae2c9c01eccccb89992
master <razafiarisonialy@gmail.com>
2020-03-14 01:02:05 +0400

    3e modification

--- int.py
+++ int.py
@@ -1,1 +1,8 @@
-krkr zan
+print("vous voulez additionner? (oui/non)")
+b = str(input())
+if b == "oui":
+    from nan import main
+if b == "non":
+    print("fin de programme")
+else:
+    print("erreur")
