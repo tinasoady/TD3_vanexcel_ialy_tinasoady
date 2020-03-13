fd0dd505a9c58441ff42a7128e2173754155698a
tinasoady <jtinasoady@gmail.com>
2020-03-13 16:00:19 +0400

    modification

--- pytest.py
+++ pytest.py
@@ -1,7 +1,17 @@
 def main():
     s=0
-    for i in range(0,2):
-        x = int(input("a= "))
-        s = s+ x
-    print("la somme est :", s)
+    print ("argument = ", end='')
+    n= int(input())
+    if (n<2):
+        print ("Erreur! ne saisi pas l'argument plus de 2 ou moins")
+        return 0
+    if (n>2):
+        print ("Erreur! ne saisi pas l'argument plus de 2 ou moins")
+        return 0
+    if (n==2):
+        for i in range(1, 3):
+            print(i,"er argument est: ", end='')
+            a= int(input())
+            s= a+s
+        print(" la somme vaut : ", s) 
 main()
